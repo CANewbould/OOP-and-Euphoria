8c8a7e8b20a5f6a2c2f2dd316ef2b427d72a94e6
CANewbould <60323628+CANewbould@users.noreply.github.com>
2020-08-05 22:51:38 +0100

    v1.1.0

--- examples/sequence.ex
+++ examples/sequence.ex
@@ -3,20 +3,25 @@
 --------------------------------------------------------------------------------
 -- Notes:
 --
---
+-- Connected processes are placed together on a single line for clarity
 --------------------------------------------------------------------------------
 --/*
 --= Example using OOEU library sequence.e
 ------
---[[[Version: 1.0.0
+--[[[Version: 1.1.0
 --Euphoria Versions: 3.1.1 and later
 --Author: C A Newbould
 --Date: 2020.08.05
 --Status: operational; incomplete
 --Changes:]]]
---* created
---* ##length## tested
---* Upper and Lower cases tested
+--* extended version of ##show## utilised
+--* ##foldr## exemplified
+--* ##filter## exemplified
+--* ##Sequence##(o,i) utilised
+--* **Vector** utilised
+--* ##Vector## utilised
+--* local Head class defined
+--* ##reverse## tested
 --
 ------
 --== Module holding examples of Sequence Objects
@@ -45,8 +50,9 @@ include sequence.e
 --=== Variables
 --
 --------------------------------------------------------------------------------
-String Len Len = "The length is: "
+String Filter Filter = "Even numbers between 10 and 20: "
 String Low Low = "Transform to Lower-Case: "
+String Rev Rev = "Evens in descending order: "
 String Seq Seq = "The sequence is: "
 String Upp Upp = "Transform to Upper-Case: "
 --------------------------------------------------------------------------------
@@ -54,29 +60,57 @@ String Upp Upp = "Transform to Upper-Case: "
 --=== Routines
 --
 --------------------------------------------------------------------------------
-function toLower(Character c)
+function toLower(Character c) : character
     return c.lower()
 end function
 constant DOWN = routine_id("toLower")
 --------------------------------------------------------------------------------
-function toUpper(Character c)
+function toUpper(Character c) : character
     return c.upper()
 end function
 constant UP = routine_id("toUpper")
 --------------------------------------------------------------------------------
-procedure main(String title)
-    String dashes
+function add(integer acc, integer n) : integer
+    return acc + n
+end function
+constant ADD = routine_id("add")
+--------------------------------------------------------------------------------
+function even(Integer n) : boolean
+    return (n.mod(2) = 0)
+end function
+constant EVEN = routine_id("even")
+--------------------------------------------------------------------------------
+--
+--=== Classes
+--
+--------------------------------------------------------------------------------
+euclass Head(String self)
+    procedure heading()
+        String dashes
+        integer t
+        t = length(this)
+        dashes = Sequence('-', t)
+        dashes.show(LF) this.show(LF) dashes.show(LF)
+    end procedure
+end euclass
+--------------------------------------------------------------------------------
+--
+--=== Main routine
+--
+--------------------------------------------------------------------------------
+procedure main(Head title)
     Integer l
-    integer t
-    String s, transformed
-    t = length(title)
-    dashes = repeat('-', t)
-    dashes.show(LF) title.show(LF) dashes.show(LF)
-    s = String("I am the way, the truth and the light!")
-    Seq.show() s.show(LF)
-    Len.show() l = s.length() l.show()
+    Vector list
+    String s
+    String transformed
+    title.heading()
+    Seq.show() s = String("I am the way, the truth and the light!") s.show(LF)
+    l = s.length() l.show("The length is: %d\n")
     Upp.show() transformed = s.map(UP) transformed.show(LF)
     Low.show() transformed = s.map(DOWN) transformed.show(LF)
+    list = Vector(1, 10) l = list.foldr(ADD, 0) l.show("The sum of [1..10] is %d\n")
+    Filter.show() list = 2*list list = list.filter(EVEN) list.show()
+    list = list.reverse() Rev.show() list.show()
 end procedure
 --------------------------------------------------------------------------------
 -- Execution
@@ -85,4 +119,13 @@ main("Testing Sequence library")
 --------------------------------------------------------------------------------
 -- Previous versions
 --------------------------------------------------------------------------------
+--[[[Version: 1.0.0
+--Euphoria Versions: 3.1.1 and later
+--Author: C A Newbould
+--Date: 2020.08.05
+--Status: operational; incomplete
+--Changes:]]]
+--* created
+--* ##length## tested
+--* Upper and Lower cases tested
 --------------------------------------------------------------------------------
