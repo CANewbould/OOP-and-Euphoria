5ba79ca7f5c55281e369e159e2841877dda1999c
CANewbould <60323628+CANewbould@users.noreply.github.com>
2020-08-10 22:35:25 +0100

    v1.2.0

--- examples/sequence.ex
+++ examples/sequence.ex
@@ -8,20 +8,13 @@
 --/*
 --= Example using OOEU library sequence.e
 ------
---[[[Version: 1.1.0
+--[[[Version: 1.2.0
 --Euphoria Versions: 3.1.1 and later
 --Author: C A Newbould
---Date: 2020.08.05
+--Date: 2020.08.08
 --Status: operational; incomplete
 --Changes:]]]
---* extended version of ##show## utilised
---* ##foldr## exemplified
---* ##filter## exemplified
---* ##Sequence##(o,i) utilised
---* **Vector** utilised
---* ##Vector## utilised
---* local Head class defined
---* ##reverse## tested
+--* added an App definition & moved former 'Head' into it
 --
 ------
 --== Module holding examples of Sequence Objects
@@ -40,6 +33,7 @@ include sequence.e
 --=== Constants
 --
 --------------------------------------------------------------------------------
+constant M_INSTANCE = 55
 --------------------------------------------------------------------------------
 --
 --=== Types
@@ -84,40 +78,51 @@ constant EVEN = routine_id("even")
 --=== Classes
 --
 --------------------------------------------------------------------------------
-euclass Head(String self)
-    procedure heading()
+--*** v1.2.0 starts
+euclass App(atom self)
+    function App()
+        return machine_func(M_INSTANCE, 0)
+    end function
+    procedure main(String title)
+        Integer l
+        Vector list
+        String s
+        String transformed
         String dashes
-        integer t
-        t = length(this)
-        dashes = Sequence('-', t)
-        dashes.show(LF) this.show(LF) dashes.show(LF)
+        dashes = Sequence('-', length(title))
+        dashes.show(LF) title.show(LF) dashes.show(LF)
+        Seq.show() s = String("I am the way, the truth and the light!") s.show(LF)
+        l = s.length() l.show("The length is: %d\n")
+        Upp.show() transformed = s.map(UP) transformed.show(LF)
+        Low.show() transformed = s.map(DOWN) transformed.show(LF)
+        list = Vector(1, 10) l = list.foldr(ADD, 0) l.show("The sum of [1..10] is %d\n")
+        Filter.show() list = 2*list list = list.filter(EVEN) list.show()
+        list = list.reverse() Rev.show() list.show()
     end procedure
 end euclass
 --------------------------------------------------------------------------------
---
---=== Main routine
---
---------------------------------------------------------------------------------
-procedure main(Head title)
-    Integer l
-    Vector list
-    String s
-    String transformed
-    title.heading()
-    Seq.show() s = String("I am the way, the truth and the light!") s.show(LF)
-    l = s.length() l.show("The length is: %d\n")
-    Upp.show() transformed = s.map(UP) transformed.show(LF)
-    Low.show() transformed = s.map(DOWN) transformed.show(LF)
-    list = Vector(1, 10) l = list.foldr(ADD, 0) l.show("The sum of [1..10] is %d\n")
-    Filter.show() list = 2*list list = list.filter(EVEN) list.show()
-    list = list.reverse() Rev.show() list.show()
-end procedure
---------------------------------------------------------------------------------
 -- Execution
 --------------------------------------------------------------------------------
-main("Testing Sequence library")
+App seqeg seqeg = App() -- instantiate
+seqeg.main("Testing Sequence library") -- execute 'main'
+--*** v1.2.0 ends
 --------------------------------------------------------------------------------
 -- Previous versions
+--------------------------------------------------------------------------------
+--[[[Version: 1.1.0
+--Euphoria Versions: 3.1.1 and later
+--Author: C A Newbould
+--Date: 2020.08.05
+--Status: operational; incomplete
+--Changes:]]]
+--* extended version of ##show## utilised
+--* ##foldr## exemplified
+--* ##filter## exemplified
+--* ##Sequence##(o,i) utilised
+--* **Vector** utilised
+--* ##Vector## utilised
+--* local Head class defined
+--* ##reverse## tested
 --------------------------------------------------------------------------------
 --[[[Version: 1.0.0
 --Euphoria Versions: 3.1.1 and later
