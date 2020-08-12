cd22dabb1b0cef225830ea4343b5b1b33ded91ed
CANewbould <60323628+CANewbould@users.noreply.github.com>
2020-08-12 16:29:56 +0100

    v1.1.0

--- examples/atom.ex
+++ examples/atom.ex
@@ -8,18 +8,16 @@
 --/*
 --= Example using OOEU library atom.e
 ------
---[[[Version: 1.0.0
+--[[[Version: 1.1.0
 --Euphoria Versions: 3.1.1 and later
 --Author: C A Newbould
---Date: 2020.08.06
+--Date: 2020.08.12
 --Status: operational; incomplete
 --Changes:]]]
 --* created
---* test for ##abs## added
---* test for ##sign## added
---* test for ##div## added
---* test for ##mod## added
---* test for ##upper## added
+--* test for ##power## added
+--* test for ##square## added
+--* test for ##sqrt## added
 --
 ------
 --== Module holding examples of Atom Objects
@@ -76,24 +74,16 @@ procedure main(Head title)
     Atom a, a_
     Character c, c_
     Integer i, i_
+    complex s
     title.heading()
-    a = Atom(-4.6)
-    a.show("The absolute value of %.1f is ")
-    a_ = a.abs()
-    a_.show()
-    a.show("The sign of %.1f is ")
-    a_ = a.sign()
-    a_.show()
-    i = Integer(13)
-    i_ = 4
-    i.show("%d (integer) divided by ")
-    i_.show("%d = ") ?i.div(i_)
-    i.show("%d mod ")
-    i_.show("%d = ") ?i.mod(i_)
-    c = Character('s')
-    c.show("Upper-case '%s' = ")
-    c_ = c.upper()
-    c_.show("%s")
+    a = Atom(-4.6) a.show("The absolute value of %.1f is ") a_ = a.abs() a_.show()
+    a.show("The sign of %.1f is ") a_ = a.sign() a_.show()
+    a_ = a.square() a.show("The square of %.1f") a_.show(" is %.2f\n")
+    a_ = a.power(3) a.show("The cube of %.1f") a_.show(" is %.2f\n")
+    s = a.sqrt() a.show("The square root of %.1f") a_ = s[2] a_.show(" is %.2fi\n")
+    i = Integer(13) i.show("%d integer-divided by ") i_ = Integer(4) i_.show("%d = ") ?i.div(i_)
+    i.show("%d mod ") i_.show("%d = ") ?i.mod(i_)
+    c = Character('s') c.show("Upper-case '%s' = ") c_ = c.upper() c_.show("'%s'")
 end procedure
 --------------------------------------------------------------------------------
 -- Execution
@@ -102,4 +92,16 @@ main("Testing Atom library")
 --------------------------------------------------------------------------------
 -- Previous versions
 --------------------------------------------------------------------------------
+--[[[Version: 1.0.0
+--Euphoria Versions: 3.1.1 and later
+--Author: C A Newbould
+--Date: 2020.08.06
+--Status: operational; incomplete
+--Changes:]]]
+--* created
+--* test for ##abs## added
+--* test for ##sign## added
+--* test for ##div## added
+--* test for ##mod## added
+--* test for ##upper## added
 --------------------------------------------------------------------------------
